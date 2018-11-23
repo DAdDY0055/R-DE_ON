dc0ba06e6599decc7cca04f5b8e31e3475ac9638
Takuya <yky2800.rthm.55tn@gmail.com>
2018-11-23 17:27:31 +0900

    Changed to spots using resource method

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'spots#index'
 
+  resources :spots
+  
   get 'users/show'
-  get 'spots/index'
-  get 'spots/new'
-  get 'spots/show'
-  get 'spots/edit'
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
