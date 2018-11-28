bf09e59be987c9813c9ecdcb0415429c7edf877e
Takuya <yky2800.rthm.55tn@gmail.com>
2018-11-28 11:47:31 +0900

    新規登録画面の日本語化、登録時にname,iconも登録できるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  mount_uploader :icon, ImageUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
