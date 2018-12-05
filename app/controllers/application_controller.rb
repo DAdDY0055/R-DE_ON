2ca34abdcf45b2dcfc208f082575040a6aacd0f8
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-06 07:46:56 +0900

    アイコン画像をgithubにアップしないよう、gitignoreに追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
+    devise_parameter_sanitizer.permit(:sign_up, :account_update, keys: [:name, :icon])
   end
 end
