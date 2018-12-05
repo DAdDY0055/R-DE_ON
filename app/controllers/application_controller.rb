fbb4344609aaf4a63f55f4bb42d889d615816969
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-05 23:55:37 +0900

    ユーザ編集画面を編集(name,iconを編集できるように+日本語化)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon])
   end
 end
