1b634bc59e248052c07bda7b9884ba62fa4e7b2b
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-14 13:34:16 +0900

    マイページにてお気に入り登録した+自分の投稿したスポットのみ表示するよう修正 close #4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @spot = Spot.all
-    @hash = Gmaps4rails.build_markers(@spot) do |spot, marker|
+    @favorite_spot = @user.favorites_spots
+    @user_spot = @user.spots
+    # プロフィール上のマップにはお気に入り + 自分の投稿を表示するよう、@hashに追加
+    @hash = Gmaps4rails.build_markers(@favorite_spot + @user_spot) do |spot, marker|
       marker.lat spot.latitude
       marker.lng spot.longitude
       marker.infowindow spot.spot_name
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :spots
   has_many :favorites, dependent: :destroy
+  has_many :favorites_spots, through: :favorites, source: :spot
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
