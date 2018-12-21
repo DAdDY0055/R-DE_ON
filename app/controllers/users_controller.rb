91fae53ff9ed2114cab037b96af328c2f1789512
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-21 17:21:08 +0900

    ログインしていないとユーザ情報を参照できないよう修正 fix #18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
   def show
     @user = User.find(params[:id])
     @favorite_spot = @user.favorites_spots
