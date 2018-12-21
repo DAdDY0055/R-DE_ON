d220a1b99eb5e56c0f097f88458c105048dcb148
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-21 15:49:34 +0900

    geocoaderで位置情報→住所に変更する際、日本後で住所登録するよう修正 fix #2

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,7 +3,7 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google,              # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
+  language: :ja,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
