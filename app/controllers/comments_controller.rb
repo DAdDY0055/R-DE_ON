e28c6d56feb37b3ec31d34d42d440b49e76d9894
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-23 20:19:59 +0900

    コメントをログインしていないとできないように修正 fix #23

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def create
     @spot = Spot.find(params[:spot_id])
     @comment = @spot.comments.build(comment_params)
