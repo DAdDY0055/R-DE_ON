cbda40f845aa9fab3fe60a28ade8ddad7f2b880d
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-23 20:36:10 +0900

    メイラーのホストを修正 fix #24

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'r-de-on' }
+  config.action_mailer.default_url_options = { host: 'r-de-on.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
