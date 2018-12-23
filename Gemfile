55315c6d4edd81074a4c90e93eaccf13b64ac586
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-23 14:31:36 +0900

    画像をS3へアップロードするように修正

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'geocoder'
 gem 'exifr'
 gem 'gmaps4rails'
 
+gem 'fog-aws'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,28 @@
+CarrierWave.configure do |config|
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
+
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                'ap-northeast-1',
+      path_style:            true,
+  }
+
+
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'public, max-age=86400'}
+
+  case Rails.env
+    when 'production'
+      config.fog_directory = 'r-de-on-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/r-de-on-production'
+    when 'development'
+      config.fog_directory = 'r-de-on-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/r-de-on-development'
+  end
+end
