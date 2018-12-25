8cf10a6c1f29e443c95a29c7173dad0088dc96ba
Takuya <yky2800.rthm.55tn@gmail.com>
2018-12-25 16:15:35 +0900

    ファイルの終端には改行や不要な改行を削除

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,7 +7,6 @@ class Spot < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   has_many :comments, dependent: :destroy
   
-
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
