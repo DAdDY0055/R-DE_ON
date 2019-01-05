010b22e248c6d20070a970cd880013197cf6d984
Takuya <yky2800.rthm.55tn@gmail.com>
2019-01-05 22:18:28 +0900

    お気に入り登録・解除時のメッセージ修正 fix #40

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(spot_id: params[:spot_id])
-    redirect_to spots_url, notice: "#{favorite.spot.user.name}さんのブログをお気に入り登録しました"
+    redirect_to spots_url, notice: "#{favorite.spot.user.name}さんの投稿をお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to spots_url, notice: "#{favorite.spot.user.name}さんのブログをお気に入り解除しました"
+    redirect_to spots_url, notice: "#{favorite.spot.user.name}さんの投稿のお気に入りを解除しました"
   end
 end
